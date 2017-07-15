e490a210f418d997de65c0cf86fa9ee4c0da5895
ADMIN <ADMIN@ADMIN-PC>
2017-07-15 08:50:12 +0530

    Assignment 1.1

---
+++ src/assignment/HelloWorld.java
@@ -0,0 +1,14 @@
+package assignment;
+
+
+//This program is to create Hello World File
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Sysste.out.println - Prints the output on console
+		System.out.println("Hello World");
+			}
+
+}
